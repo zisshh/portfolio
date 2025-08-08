148eafd85d17e8f43a34a028525ca4d127dd056d
Div <126823294+zisshh@users.noreply.github.com>
2025-08-08 08:06:18 +0530

    Update route.js

--- app/api/download-cv/route.js
+++ app/api/download-cv/route.js
@@ -3,13 +3,13 @@ import fs from 'fs';
 import path from 'path';
 
 export async function GET() {
-  const filePath = path.join(process.cwd(), 'public', 'cv.pdf');
+  const filePath = path.join(process.cwd(), 'public', 'thkr_current_resume.pdf');
   if (fs.existsSync(filePath)) {
     const file = fs.readFileSync(filePath);
     return new NextResponse(file, {
       headers: {
         'Content-Type': 'application/pdf',
-        'Content-Disposition': 'attachment; filename="cv.pdf"'
+        'Content-Disposition': 'attachment; filename="thkr_current_resume.pdf"'
       }
     });
   }
